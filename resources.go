f1708abd5044151c550bfd2ac3a8e9b219da9e19
lixhu <jackliahu@gmail.com>
2025-03-18 15:22:40 +0800

    refactor: 移除未使用的SVG文件嵌入

--- resources.go
+++ resources.go
@@ -5,5 +5,5 @@ import "embed"
 //go:embed resources/fonts/SourceHanSansCN-Regular.ttf
 var chineseFontFS embed.FS
 
-//go:embed resources/images/enemy.svg resources/images/player.svg resources/images/enemy.png resources/images/player.png
+//go:embed resources/images/enemy.png resources/images/player.png
 var imagesFS embed.FS
